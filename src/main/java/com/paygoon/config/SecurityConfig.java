faa01e9e5487dac7aeaa4f161ad81f3c9ab8bb64
Enrique <eimbertbc@gmail.com>
2026-02-15 21:07:40 +0100

    varios

--- src/main/java/com/paygoon/config/SecurityConfig.java
+++ src/main/java/com/paygoon/config/SecurityConfig.java
@@ -39,7 +39,6 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(
                         "/auth/**",
-                        "/api/auth/**",
                         "/",
                         "/index.html",
                         "/register.html",
